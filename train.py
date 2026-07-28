f989d0c5cc14e4ae4899ba96e6d1403dd4ebfb95
Mone <monetgirma2018@gmail.com>
2026-07-28 18:02:14 +0300

    Add training reward graph visualization

--- train.py
+++ train.py
@@ -1,19 +1,27 @@
 from environment import GridWorld
 from agent import Agent
 import numpy as np
+import matplotlib.pyplot as plt
 
 env = GridWorld()
 agent = Agent()
 
 episodes = 100
 
+# Reward history
+reward_history = []
+
 for episode in range(episodes):
 
     state = env.reset()
+
     done = False
 
     max_steps = 100
     steps = 0
+
+    # Total reward for one episode
+    total_reward = 0
 
     while not done and steps < max_steps:
 
@@ -29,7 +37,13 @@ for episode in range(episodes):
         )
 
         state = next_state
+
+        # Add reward
+        total_reward += reward
+
         steps += 1
+
+    reward_history.append(total_reward)
 
     agent.decay_epsilon()
 
@@ -38,3 +52,18 @@ print("Training Finished!")
 np.save("q_table.npy", agent.q_table)
 
 print("Q-table Saved!")
+
+# Reward Graph
+plt.figure(figsize=(8,5))
+
+plt.plot(reward_history)
+
+plt.title("Training Reward")
+
+plt.xlabel("Episode")
+
+plt.ylabel("Total Reward")
+
+plt.grid(True)
+
+plt.show()
