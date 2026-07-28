fd8d19a96932ad74adb6e556ac86729df0a16b51
Mone <monetgirma2018@gmail.com>
2026-07-28 17:44:26 +0300

    Load trained Q-table and successfully solve maze

--- test.py
+++ test.py
@@ -1,22 +1,24 @@
 from environment import GridWorld
 from agent import Agent
+import numpy as np
 
 env = GridWorld()
 agent = Agent()
 
+# Load trained Q-table
+agent.q_table = np.load("q_table.npy")
+
+# Disable exploration
 agent.epsilon = 0
 
 state = env.reset()
 
 done = False
 
-print("Start:", state)
-
-# ------------------------
-# Kana dabali
-# ------------------------
 max_steps = 100
 steps = 0
+
+print("Start:", state)
 
 while not done and steps < max_steps:
 
@@ -24,13 +26,13 @@ while not done and steps < max_steps:
 
     next_state, reward, done = env.step(action)
 
-    print("Action:", action)
-    print("State:", next_state)
+    print(f"Action: {action} -> State: {next_state}")
 
     state = next_state
+
     steps += 1
 
 if done:
-    print("Goal Reached!")
+    print("🎉 Goal Reached!")
 else:
-    print("Maximum steps reached.")
+    print("❌ Maximum steps reached.")
